097a30ae1119ce0147f4e6c5b9eaf08ccae737a4
paripal <paripal5z34@gmail.com>
2022-06-24 19:01:57 +0900

    event

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,12 @@
 
 const double k_max_speed = 0.5;
 const double k_max_turn_speed = 0.5;
-
-CRGB led[1];
-
-int16_t in_data[3];
 const uint32_t MD_ID = 0x01;
 const uint32_t sol_ID = 0x06;
+
+CRGB led[1];
 int16_t md_data[4] = {0, 0, 0, 0};
+hw_timer_t* tim_md_update;
 
 void update_led(uint8_t R, uint8_t G, uint8_t B){
   led[0] = G << 16 | R << 8 | B;
@@ -44,6 +43,11 @@ int update_md(uint32_t id, int16_t data[4]){
 
 }
 
+void md_update_timeout(){
+  update_md(MD_ID,md_data);
+  return;
+}
+
 int update_sol(uint32_t id, uint8_t data){
   #if defined(CAN_ds)
   return 1;
@@ -56,30 +60,14 @@ int update_sol(uint32_t id, uint8_t data){
   #endif
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  FastLED.addLeds<SK6812, LED_PIN, RGB>(led, 1); //GRB order
-  update_led(0, 0, 0);
-  uint8_t btmac[6];
-  esp_read_mac(btmac, ESP_MAC_BT);
-  Serial.printf("[Bluetooth] Mac Address = %02X:%02X:%02X:%02X:%02X:%02X\r\n", btmac[0], btmac[1], btmac[2], btmac[3], btmac[4], btmac[5]);
-  PS4.begin("4C:75:25:92:31:52");
-  Serial.println("Ready.");
-  update_led(128, 0, 0);
-  CAN.setPins(19, 22);
-  CAN.begin(1e6);
-  update_led(0, 128, 0);
-}
+void controller_event(ps4_t ps4, ps4_event_t event){
+  if(event.analog_move.stick.rx || event.analog_move.stick.ry || event.analog_move.stick.lx){
+    
+    int16_t in_data[3];
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if (PS4.isConnected()) {
-    update_led(0, 0, 128);
-
-    in_data[0] = PS4.RStickX() << 8;
-    in_data[1] = PS4.RStickY() << 8;
-    in_data[2] = PS4.LStickX() << 8;
+    in_data[0] = ps4.analog.stick.rx << 8;
+    in_data[1] = ps4.analog.stick.ry << 8;
+    in_data[2] = ps4.analog.stick.lx << 8;
 
     md_data[0] = k_max_speed * (in_data[0] * cos(PI*2.0/3.0) + in_data[1] * sin(PI*2.0/3.0))
       + k_max_turn_speed * in_data[2];
@@ -88,9 +76,45 @@ void loop() {
     md_data[2] = k_max_speed * in_data[0]
       + k_max_turn_speed * in_data[2];
     update_md(MD_ID, md_data);
+  }
+  if(event.button_up.circle){
+    update_sol(sol_ID, 0b10101010);
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  //LED init
+  FastLED.addLeds<SK6812, LED_PIN, RGB>(led, 1); //GRB order
+  update_led(0, 0, 0);
+  //bluetooth for PS4 init
+  uint8_t btmac[6];
+  esp_read_mac(btmac, ESP_MAC_BT);
+  Serial.printf("[Bluetooth] Mac Address = %02X:%02X:%02X:%02X:%02X:%02X\r\n", btmac[0], btmac[1], btmac[2], btmac[3], btmac[4], btmac[5]);
+  PS4.begin("4C:75:25:92:34:9E");
+  ps4SetEventCallback(&controller_event);
+  Serial.println("Ready.");
+  //CAN init
+  CAN.setPins(19, 22);
+  CAN.begin(1e6);
+  //timer init
+  tim_md_update = timerBegin(1, 80, true);
+  timerAttachInterrupt(tim_md_update, &md_update_timeout, true);
+  timerAlarmWrite(tim_md_update, 10e3, true);
+  //set LED green if succese all initialize
+  update_led(0, 128, 0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (PS4.isConnected()) {
+    update_led(0, 0, 128);
+
+    
 
     if(PS4.Circle()){
-      update_sol(sol_ID, 0b10101010);
+      
     }else if(PS4.Cross()){
       update_sol(sol_ID, 0b01010101);
     }else{
@@ -113,7 +137,6 @@ void loop() {
     md_data[1] = 0;
     md_data[2] = 0;
     md_data[3] = 0;
-    update_md(MD_ID, md_data);
     update_led(0, 128, 0);
     Serial.println("Controller not connected.");
   }
