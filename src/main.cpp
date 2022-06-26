6150c98c4723f6cc2be3ef640b5e01d24e140eb0
paripal <paripal5z34@gmail.com>
2022-06-27 01:27:17 +0900

    making speed pid

--- src/main.cpp
+++ src/main.cpp
@@ -8,62 +8,37 @@
 #define LED_PIN 27
 
 typedef int16_t md_data_typedef[4];
+
+typedef struct{
+  float x;
+  float y;
+  float z;
+}vector3f_typedef;
+
 typedef struct{
   uint8_t state1;
   uint8_t state2;
   uint32_t delay_time;
 }sol_pattern_typedef;
 
+typedef struct{
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+}RGB_typedef;
+
 QueueHandle_t queueHandle_run;
 QueueHandle_t queueHandle_md;
 QueueHandle_t queueHandle_sol;
-
-CRGB led[1];
-
-void update_led(CRGB* led, uint8_t R, uint8_t G, uint8_t B){
-  led[0] = G << 16 | R << 8 | B;
-  FastLED.show();
-  return;
-}
-
-int update_md(uint32_t id, int16_t data[4]){
-  
-  //debug
-  Serial.printf("id : %d\n", id);
-  Serial.printf("data : %d, %d, %d, %d\n", data[0], data[1], data[2], data[3]);
-  #if defined(CAN_ds)
-  return 1;
-  #else
-  if(!CAN.beginPacket(id, 8)){
-    return 0;
-  }
-  for (int i = 0; i < 4; i++) {
-    CAN.write(data[i] & 0xFF);
-    CAN.write(data[i] >> 8 & 0xFF);
-  }
-
-  return CAN.endPacket();
-  #endif
-
-}
-
-int update_sol(uint32_t id, uint8_t data){
-  #if defined(CAN_ds)
-  return 1;
-  #else
-  if(!CAN.beginPacket(id, 1)){
-    return 0;
-  }
-  CAN.write(data);
-  return CAN.endPacket();
-  #endif
-}
+QueueHandle_t queueHandle_led;
+QueueHandle_t queueHandle_speed_cur;
+QueueHandle_t queueHandle_speed_tar;
 
 //task
 void task_md_update(void *id) {
   int run = 0;
   md_data_typedef output = {0, 0, 0, 0};
-  delay(500);
+  delay(50);
   while (1) {
     xQueueReceive(queueHandle_md,&output,100);
     xQueuePeek(queueHandle_run, &run,0);
@@ -85,7 +60,7 @@ void task_md_update(void *id) {
 void task_sol_update(void *id) {
   int run = 0;
   sol_pattern_typedef output = {0x00, 0x00, 0};
-  delay(500);
+  delay(50);
   while (1) {
     xQueueReceive(queueHandle_sol,&output,portMAX_DELAY);
     xQueuePeek(queueHandle_run, &run,0);
@@ -108,6 +83,33 @@ void task_sol_update(void *id) {
   }
 }
 
+void task_stamp_led(void *arg) {
+  RGB_typedef rgb = {0, 0, 0};
+  CRGB led[1];
+
+  //LED init
+  FastLED.addLeds<SK6812, LED_PIN, RGB>(led, 1); //GRB order
+  led[0] = rgb.green << 16 | rgb.red << 8 | rgb.blue;
+  FastLED.show();
+
+  while(1){
+    xQueueReceive(queueHandle_led, &rgb, portMAX_DELAY);
+    led[0] = rgb.green << 16 | rgb.red << 8 | rgb.blue;
+    FastLED.show();
+    delay(50);
+  }
+}
+
+void task_speed_pid(void *arg) {
+  vector3f_typedef speed_current;
+  vector3f_typedef speed_target;
+  while(1){
+    xQueueReceive(queueHandle_speed_cur, &speed_current, 100);
+    xQueueReceive(queueHandle_speed_tar, &speed_target, 0);
+    delay(1);
+  }
+}
+
 void setup() {}
 
 void loop() {
@@ -116,6 +118,7 @@ void loop() {
 
   TaskHandle_t taskHandle_md_update;
   TaskHandle_t taskHandle_sol_update;
+  TaskHandle_t taskHandle_stamp_led;
 
   //enable core1 WTD
   enableCore1WDT();
@@ -124,44 +127,38 @@ void loop() {
   Serial.begin(115200);
   Serial.println("reseted");
 
-  //LED init
-  FastLED.addLeds<SK6812, LED_PIN, RGB>(led, 1); //GRB order
-  update_led(led, 0, 0, 0);
+  //task create
+  xTaskCreateUniversal(task_md_update,  "task_md_update",   8192, (void*)&MD_ID,  3,  &taskHandle_md_update,  APP_CPU_NUM);
+  xTaskCreateUniversal(task_sol_update, "task_sol_update",  8192, (void*)&sol_ID, 1,  &taskHandle_sol_update, APP_CPU_NUM);
+  xTaskCreateUniversal(task_stamp_led,  "task_stamp_led",   8192, NULL,           1,  &taskHandle_stamp_led,  APP_CPU_NUM);
+
+  //queue create
+  queueHandle_md  = xQueueCreate(1, sizeof(md_data_typedef));
+  queueHandle_sol = xQueueCreate(1, sizeof(sol_pattern_typedef));
+  queueHandle_run = xQueueCreate(1, sizeof(int));
+  queueHandle_led = xQueueCreate(1, sizeof(RGB_typedef));
 
   //CAN init
   CAN.setPins(19, 22);
   CAN.begin(1e6);
+  
+  //CAN event callback def
+  CAN.onReceive([](int packetSize){
+    const long id_sensor_board0_0 = 9;
+    const long id_sensor_board0_1 = 10;
+    if(CAN.packet_id == id_sensor_board0_0){
+      
+    }
+    if(CAN.packet_id == id_sensor_board0_1){
 
-  //task create
-  xTaskCreateUniversal(
-    task_md_update,
-    "task_md_update",
-    8192,
-    (void*)&MD_ID,
-    1,
-    &taskHandle_md_update,
-    APP_CPU_NUM
-  );
-  xTaskCreateUniversal(
-    task_sol_update,
-    "task_sol_update",
-    8192,
-    (void*)&sol_ID,
-    1,
-    &taskHandle_sol_update,
-    APP_CPU_NUM
-  );
-
-  //queue create
-  queueHandle_md = xQueueCreate(1, sizeof(md_data_typedef));
-  queueHandle_sol = xQueueCreate(1, sizeof(sol_pattern_typedef));
-  queueHandle_run = xQueueCreate(1, sizeof(int));
+    }
+  });
 
   //bluetooth for PS4 init
   uint8_t btmac[6];
   esp_read_mac(btmac, ESP_MAC_BT);
   Serial.printf("[Bluetooth] Mac Address = %02X:%02X:%02X:%02X:%02X:%02X\r\n", btmac[0], btmac[1], btmac[2], btmac[3], btmac[4], btmac[5]);
-  PS4.begin("4C:75:25:92:31:52");
+  PS4.begin("4C:75:25:92:31:52");   //set bluetooth MAC address(see this↑ with serial monitor) here
   
   //PS4 event callback def
   ps4SetEventCallback([](ps4_t ps4, ps4_event_t event){
@@ -197,22 +194,39 @@ void loop() {
       xQueuePeek(queueHandle_run, &run, 0);
       run = !run;
       xQueueOverwrite(queueHandle_run, &run);
+
+      ps4_cmd_t controller = {0, 0, 0, 0, 0, 0, 0};
+      if(run){
+        controller.r = 0;
+        controller.g = 0;
+        controller.b = 255;
+      }else{
+        controller.r = 255;
+        controller.g = 0;
+        controller.b = 0;
+        controller.flashOn = 16;
+        controller.flashOff = 16;
+      }
+      ps4SetOutput(controller);
     }
   });
   ps4SetConnectionCallback([](uint8_t isConnected){
     if(isConnected){
+      RGB_typedef blue = {0, 0, 128};
       PS4.setLed(0, 0, 255);
-      update_led(led, 0, 0, 128);
+      xQueueOverwrite(queueHandle_led, &blue);
     }else{
+      RGB_typedef red = {128, 0, 0};
       md_data_typedef data = {0, 0, 0, 0};
       xQueueOverwrite(queueHandle_md, data);
-      update_led(led, 0, 128, 0);
+      xQueueOverwrite(queueHandle_led, &red);
     }
   });
   Serial.println("Ready.");
 
   //set LED green if succese all initialize
-  update_led(led, 0, 128, 0);
+  RGB_typedef green = {0, 128, 0};
+  xQueueOverwrite(queueHandle_led, &green);
 
   while(1){
     // put your main code here, to run repeatedly:
@@ -223,7 +237,7 @@ void loop() {
       Serial.println("Controller not connected.");
     }
     //plese keep this delay bigger than 1. if not, main task stopped by WTD
-    //(WTD is enabled in src\main.cpp:87 to restart when CAN message writing failue)
+    //(WTD is enabled to restart when CAN message writing failue)
     delay(500);
   }
 }
